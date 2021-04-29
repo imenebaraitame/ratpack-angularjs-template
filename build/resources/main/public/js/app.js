f5fc1c6a5891212581e308de24ee8940a3803828
Ibrahim.H <bitsnaps@yahoo.fr>
2021-04-29 18:31:11 +0100

    add a simple ui

--- build/resources/main/public/js/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('app', []);
-
-app.controller('HomeController', function ($scope) {
-  $scope.username = 'Admin';
-});
--- src/ratpack/public/js/app.js
+++ src/ratpack/public/js/app.js
@@ -1,4 +1,21 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router', 'ngResource', 'ngCookies']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state({
+      name: 'Home',
+      url: '/',
+      template: '<h1>Home</h1>'
+    })
+    .state({
+      name: 'About',
+      url: '/about',
+      template: '<h1>About</h1>'
+    });
+
+  $urlRouterProvider.otherwise('/');
+
+});
 
 app.controller('HomeController', function ($scope) {
   $scope.username = 'Admin';
